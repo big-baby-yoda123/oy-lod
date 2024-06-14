dd00c20803db8a891567545f86cd9c1e0c7dbba2
Nadav Zilberberg <nadav2807@gmail.com>
2024-06-14 22:43:53 +0300

    added project sub dirs

--- App/src/main.rs
+++ App/src/main.rs
@@ -1,10 +1,3 @@
-use bevy::prelude::*;
-
-pub mod game;
-
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(game::GamePlugin)
-        .run();
+    println!("Hello, world!");
 }
